54b739add004b5ceba78c3591c85714062ccaf01
Nouman Aslam <mrnoumannomi7@gmail.com>
2026-02-03 00:55:26 +0500

    React children prop practice
    React children prop practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
-import UserCard from './Components/UserCard'
-import dogpic from './assets/dogpic.jpg'
-import Eaglepic from './assets/Eaglepic.jpg'
-import Foximage from './assets/Foximg.jpeg'
-import "./index.css"
+import { useState } from "react";
+import Card from "./components/Card";
+import Button from "./components/Button";
 function App() {
+  const [state,setState]=useState(5)
+  function HandleClick()
+  {
+    setState(state+1)
+  }
   return (
     <>
-      <UserCard
-        name="Eagle"
-        description="A majestic bird of prey, symbolizing freedom and vision"
-        image={Eaglepic}
-      />
-
-      <UserCard
-        name="Dog"
-        description="A loyal companion, friendly and full of energy."
-        image={dogpic}
-      />
-
-      <UserCard
-        name="Fox"
-        description="A clever and agile animal, known for its cunning nature"
-        image={Foximage}        
-      />
+      <Card name="Nouman Aslam">
+        <h1>React Course</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem recusandae quo, numquam ipsa hic placeat, quaerat cum, suscipit reiciendis dicta explicabo corporis tempore. Inventore odit ipsum, commodi voluptas temporibus maiores.</p>
+        <button>Click Me</button>
+      </Card>
+      <Card children="This is a children">
+      </Card>
+      <Button HandleClick={HandleClick} text="Click">
+        <h1>{state}</h1>
+      </Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
